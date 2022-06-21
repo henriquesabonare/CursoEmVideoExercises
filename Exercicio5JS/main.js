d8da4d50f3e68120783b074a2ba6a10fed55d386
Sabonare <henrique.sabonare@capgemini.com>
2022-06-21 15:26:46 -0300

    Exercise 5 Js finished

--- Exercicio5JS/main.js
+++ Exercicio5JS/main.js
@@ -1,33 +1,49 @@
 let array = new Array()
 
 function showResultsInAdicionar(){
-  showAlert()
-  AddNumber()
+  showAlert(array)
+  addNumber(array)
 }
-
-function showAlert(){
-
+function showAlert(arrayValue){
   const inputNumber = parseInt(document.querySelector("#number").value)
   
-  if(inputNumber > 100 || inputNumber <1 || Number.isNaN(inputNumber) || array.includes(inputNumber)){
+  if(inputNumber > 100 || inputNumber <1 || Number.isNaN(inputNumber) || arrayValue.includes(inputNumber)){
     alert("Selecione um número Válido")
   }
 }
-
-function AddNumber(){
+function addNumber(arrayValue){
   const inputNumber = parseInt(document.querySelector("#number").value)
   const result = document.querySelector("#select")
 
   if(inputNumber < 101 && inputNumber >0){
-    if(array.includes(inputNumber) == false){
+    if(arrayValue.includes(inputNumber) == false){
     result.innerHTML += `<option>Valor ${inputNumber} adicionado.</option>`
-    array.push(inputNumber)
+    arrayValue.push(inputNumber)
+    }
   }
-  }
-  
+  return array
+}
+function somar() {
+    let soma = 0;
+    for(let i in array){
+      soma += array[i]
+    } 
+    return soma
 }
 function finalizar(){
+  const resultNumbers = document.querySelector(".resultNumbers")
+  const expandContent = document.querySelector(".content")
 
-
+  if(array.length === 0){
+    return alert(`Insira ao menos um valor`)
+  }else{
+  expandContent.style.height = `105%`
+  resultNumbers.innerHTML = `
+  <p>Ao todo, temos ${array.length} números cadastrados </p>
+  <p>O maior valor informado foi  ${Math.max.apply(null, array)}.</p>
+  <p>O menor valor informado foi  ${Math.min.apply(null, array)}.</p>
+  <p>Somando todos os valores, temos ${somar()}.</p>
+  <p>A média dos valores inseridos é  ${somar()/array.length}.</p>
+  `
+  }  
 }
-console.log(array)
